180158ea04668146827d2a00b8103565120bd4aa
Omphile Morwane <omphilemorwane@gmail.com>
2024-05-10 05:46:09 +0200

    Function for validationn when values are missing

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
 
-  //Dividing numbers result in a decimal number
+  //Scenario:Dividing numbers result in a decimal number
   result.innerText = Math.floor(dividend / divider);
   result.classList.remove("error-message");
 
@@ -15,8 +15,14 @@ form.addEventListener("submit", (event) => {
     result.innerText = "Error: Invalid number provided. Try again!";
     result.classList.add("error-message");
     throw new Error (
-      "Invalid division, cannot use 0"
+      "Division not performed. Invalid number provided. Try again"
     )
+  }
+
+  //validation when values are missing
+  else if (!dividend || !divider) {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
+    result.classList.add("error-message");
   }
 
 });
